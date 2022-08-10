617b988f962dfb42e3b957c11008d8a51d943c22
hung <hung.nguyen@devplus.edu.vn>
2022-08-10 20:54:18 +0700

    receive done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Common from "./Components/Common/Common";
+import Receive from "./Components/Receive/Receive";
 
 function App() {
   return (
     <>
+      <Receive/>
       <Common />
     </>
   );
---
+++ src/Components/Receive/Receive.js
@@ -0,0 +1,128 @@
+import './Receive.css'
+
+ function Receive (){
+return(
+  <section className="receive">
+  <div className="content-title">
+   <div className="content-list">
+     <div className="list-heading">
+        <h2>What an engineer after Devplus will must have?</h2>
+     </div>
+     <div className="list-container">
+       <div className="list-row">
+         <div className="list-item">
+           <div className="list-icon">
+             <img src="https://devplus.edu.vn/assets/images/categories/icons/1.png" alt="img"/>
+           </div>
+           <div className="list-text">
+             <h4>Programing foundation</h4>
+             <span>Algorithm, Design patte</span>
+           </div>
+         </div>  
+       </div>
+
+       <div className="list-row">
+         <div className="list-item">
+           <div className="list-icon">
+             <img src="https://devplus.edu.vn/assets/images/categories/icons/2.png" alt="img"/>
+           </div>
+           <div className="list-text">
+             <h4>Database</h4>
+             <span>SQL Syntax</span>
+           </div>
+         </div>  
+      </div>
+
+      <div className="list-row ">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/3.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Testing</h4>
+           <span>Unit testing, TDD</span>
+         </div>
+       </div>  
+     </div>
+
+     <div className="list-row ">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/4.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Front-end</h4>
+           <span>HTM/CSS/JS , ReactJS/VueJS</span>
+         </div>
+       </div>  
+     </div>
+
+     <div className="list-row ">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/5.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Back-end</h4>
+           <span>PHP, NodeJS</span>
+         </div>
+       </div> 
+     </div>
+
+     <div className="list-row ">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/6.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Agile Stable Team</h4>
+           <span>Agile/Scrum</span>
+         </div>
+       </div>  
+     </div>
+
+     <div className="list-row">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/7.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>English</h4>
+           <span>Basic conversion</span>
+         </div>
+       </div>  
+     </div>
+
+     <div className="list-row">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/8.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Communication</h4>
+           <span>HORENSO</span>
+         </div>
+       </div>  
+     </div>
+
+     <div className="list-row">
+       <div className="list-item">
+         <div className="list-icon">
+           <img src="https://devplus.edu.vn/assets/images/categories/icons/9.png" alt="img"/>
+         </div>
+         <div className="list-text">
+           <h4>Career path</h4>
+           <span>Work ethic, Integrity</span>
+         </div>
+       </div>  
+     </div>
+      </div>
+
+   </div>
+</div></section> 
+                
+   
+         
+)
+}
+export default Receive
