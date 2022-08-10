b0ba8651c82b2cd95744e895ee08376c5c6ee073
hung <hung.nguyen@devplus.edu.vn>
2022-08-10 21:47:11 +0700

    fix bug campus

---
+++ src/Components/Campus/Campus.js
@@ -0,0 +1,55 @@
+import'./Campus.css'
+function Campus(){
+    return(
+        <section class="our-campus">
+        <div class="content-campus">
+          <div class="campus-container">
+            <div class="campus-heading">
+              <h2>Our main campus</h2>
+            </div>
+            <div class="campus-row">
+        
+              <div class="campus-cover fade up">
+                <div class="campus-size">
+                  <div class="campus-img">
+                    <img src="https://devplus.edu.vn/assets/images/devplus/One_plus.png" alt="one plus"/>
+                  </div>
+                  <div class="campus-title">
+                    <h3>One plus (+) Programing foundation</h3>
+                  </div>
+        
+                </div>
+        
+              </div> 
+              <div class="campus-cover fade up">
+                <div class="campus-size">
+                  <div class="campus-img">
+                    <img src="https://devplus.edu.vn/assets/images/devplus/Two_plus.png" alt="tow plus"/>
+                  </div>
+                  <div class="campus-title">
+                    <h3>Two plus (++) Skill up to to get ready</h3>
+                  </div>
+        
+                </div>
+        
+              </div>
+              <div class="campus-cover fade up">
+                <div class="campus-size">
+                  <div class="campus-img">
+                    <img src="https://devplus.edu.vn/assets/images/devplus/Three_plus.png" alt="three plus"/>
+                  </div>
+                  <div class="campus-title">
+                    <h3>Three plus (+++) How to become a senior</h3>
+                  </div>
+        
+                </div>
+        
+              </div>
+            </div>
+        
+          </div> 
+        </div>
+      </section>
+    )
+}
+export default Campus
