8f80aa71ded9847f36a03a46eb70e342d2aea92a
ngoc_ng <nmtngoc238@gmail.com>
2022-08-11 11:29:32 +0700

    done navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import "./App.css";
 import Common from "./Components/Common/Common";
 import Receive from "./Components/Receive/Receive";
 import Campus from "./Components/Campus/Campus";
+<<<<<<< HEAD
 import Review from "./Components/Review/Review";
 import AboutDev from "./Components/AbouDev/AboutDev";
 import Navbar from "./Components/Navbar/Navbar";
+=======
+import Navbar from "./Components/Navbar/Navbar";
+
+>>>>>>> done navbar
 function App() {
   return (
     <>
       <Navbar/>
+<<<<<<< HEAD
       <AboutDev />
       <Receive />
       <Campus />
+=======
+      <Receive/>
+      <Campus/>
+>>>>>>> done navbar
       <Common />
       <Review />
     </>
